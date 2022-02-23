2a0793fe748c8ff91d5ee21b7bc8a75db24da4cb
Moderatuh <91396821+Moderatuh@users.noreply.github.com>
2022-02-22 19:16:32 -0500

    Update runVoteBot.bat

--- voteBot/runVoteBot.bat
+++ voteBot/runVoteBot.bat
@@ -1,17 +1,10 @@
 @echo off
 
 SET count=1
-FOR /L %%G IN (0,1,100) DO (
+FOR /L %%G IN (0,1,10000) DO (
 
-call :subroutine "%%G"
 node voteBot.js
 
-IF /I "%%G" EQU "23" (
-
-call :sub "%%G"
-
-timeout 5
-)
 
 timeout 2
 
